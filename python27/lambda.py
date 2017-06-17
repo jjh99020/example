6505a71a14a672a283285702990a5ad1650d274d
Jeon JongHwa <dap99020@naver.com>
2017-06-17 13:05:04 +0900

    korean remark change
    korean remark change

--- python27/lambda.py
+++ python27/lambda.py
@@ -2,19 +2,19 @@ def hap(x,y):
     return x+y
 print hap(10,20)
 
-#lambda 형식
+#lambda method
 print (lambda x,y:x+y)(10,20)
 
-#map(함수,리스트) 리스트의 원소를 함수에 하나씩 적
-#**제곱근 인듯..
+#map(method,list) map method use the variables in the list sequentially
+
 print map(lambda x: x**2, range(5))
-print list(map(lambda x: x**2, range(5)))#파이썬 3에선 리스트화 해줘야
+print list(map(lambda x: x**2, range(5)))#python3 
 
-#reduce(함수,순서형자료)
+#reduce(method,list)
 
-from functools import reduce #파이썬3에선 필요
-print reduce(lambda x,y:x+y,[0,1,2,3,4])# 첫인자가 누적값 
-print reduce(lambda x, y: y+x, 'asdfg')#문자열도 가능 한글은 거꾸로배치하면 깨짐
+from functools import reduce #python3 
+print reduce(lambda x,y:x+y,range(5))# frist variable is accumulte variable
+print reduce(lambda x, y: y+x, 'asdfg')#String is also possible. If you change back and forth, Korean is broken 
 
-#filter(함수,리스트) 참인값만 출력
+#filter(method,list) ture state return
 print filter(lambda x:x<5, range(10))
